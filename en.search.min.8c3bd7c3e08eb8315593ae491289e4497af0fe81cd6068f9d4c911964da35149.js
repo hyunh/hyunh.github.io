8fc487d434294c8ca75dd746d3ffc524b54cab78
Hyunhae Lee <hyunhae.lee@gmail.com>
2021-02-03 02:34:15 +0900

    new post in hugo and update theme

--- en.search.min.8c3bd7c3e08eb8315593ae491289e4497af0fe81cd6068f9d4c911964da35149.js
+++ en.search.min.8c3bd7c3e08eb8315593ae491289e4497af0fe81cd6068f9d4c911964da35149.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.92ea03653b5cea8e51e46dc90584ebae1684afa34d074e58b4587f4cd261c422.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.1b9ef47a726b2c28b56ec545916534f9e3277a9aa252c98e1ebda8c26ace2847.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
